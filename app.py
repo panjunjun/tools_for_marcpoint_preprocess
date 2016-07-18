97a67792ab9dc2eff87482936c2fce5704bd8fdf
panjunjun <41317139@qq.com>
2016-07-19 01:55:02 +0800

    完成界面和功能

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 # coding: utf-8
-import tornado.httpserver as httpserver
-import tornado.ioloop as ioloop
+import tornado.httpserver
+import tornado.ioloop
 import tornado.web
 from tornado.options import define, options
 from router import handler, template_path
-define("port", default=8188, help="run on given port", type=int)
+define(u"port", default=8188, help="run on given port", type=int)
 
 
-if __name__ == "__main__":
+if __name__ == u"__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=handler, template_path=template_path)
-    http_server = httpserver.HTTPServer(app)
+    app = tornado.web.Application(handlers=handler, template_path=template_path, debug=True)
+    http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     print ("web server start")
-    ioloop.IOLoop.instance().start()
+    tornado.ioloop.IOLoop.instance().start()
--- handler.py
+++ handler.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 from tornado.web import RequestHandler as BaseHandler
 import textwrap
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from preprocess.algorithm_demo import VerifyResult as VR
 
 
 class IndexHandler(BaseHandler):
@@ -35,7 +38,14 @@ class AlgorithmDemoFormHandler(BaseHandler):
 
 
 class AlgorithmDemoHandler(BaseHandler):
-    def prepare(self):
-        item1_id = self.get_argument("item1_id")
-        item2_id = self.get_argument("item2_id")
-        self.render("demo.html", item1_id=item1_id, item2_id=item2_id)
+    def post(self):
+        item1_id = int(self.get_argument("item1_id"))
+        item2_id = int(self.get_argument("item2_id"))
+        category_id = int(self.get_argument("category_id"))
+        vr = VR(source_table=u"itemmonthlysales2015",target_table=u"itemmonthlyrelation_2015",date_range=u"2015-12-01")
+        vr.main(item1_id=item1_id, item2_id=item2_id, category_id=category_id)
+        item_pair = vr.competitive_item_pair_data.values
+        item1 = item_pair[0]
+        item2 = item_pair[1]
+        dimension_list = vr.tag_dict[vr.category_id].keys()
+        self.render("demo.html", item1=item1, item2=item2, fv=vr.feature_vector, y=vr.predict_y, li=dimension_list)
